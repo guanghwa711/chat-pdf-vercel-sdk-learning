b4a7acfbef8eca24863b329eae6459c3720e6983
guanghwa711 <guanghwa711@gmail.com>
2024-10-29 15:13:41 +0200

    updated dataDir in the get files route

--- app/api/files/route.ts
+++ app/api/files/route.ts
@@ -1,10 +1,9 @@
 import { NextResponse } from "next/server";
 import fs from "fs";
-import path from "path";
 
 export async function GET() {
   try {
-    const dataDir = path.join(process.cwd(), "data");
+    const dataDir = "./data";
     if (!fs.existsSync(dataDir)) {
       return NextResponse.json({ files: [], dataDir });
     }
